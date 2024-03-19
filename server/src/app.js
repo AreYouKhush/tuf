c3486945bc135b1d56da00d95c3aa52e764951f7
AreYouKhush <khushal9587@gmail.com>
2024-03-19 21:41:51 +0530

    server/package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,55 @@
-const express = require('express');
-const cors = require('cors');
-const sequelize = require('./api/db/server');
-const User = require('./api/db/user');
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("./api/db/server");
+const User = require("./api/db/user");
 const app = express();
 
-const userRouter = require('./api/routes/user')
+const userRouter = require("./api/routes/user");
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://tuf-roan.vercel.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
+app.use(
+  cors({
+    origin: ["https://tuf-roan.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 sequelize.sync();
 
-app.get('/', async (req, res) => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-    res.json("Connected");
-})
+app.get("/", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  res.json("Connected");
+});
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
 module.exports = app;
